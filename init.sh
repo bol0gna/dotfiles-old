1960ae4eb92935417ac4f06aef41f7de1f755ccc
bol0gna <75225753+bol0gna@users.noreply.github.com>
2020-11-29 20:02:00 -0500

    init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Install config for: "
+for dir in */; do
+    echo -n "$dir (y/N) "
+    read ans
+    if [ "$ans" != "${ans#[Yy]}" ] ;then
+        stow --dotfiles $dir
+    fi
+done	
